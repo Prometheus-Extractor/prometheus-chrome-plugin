3927ab976aa6c1ec8d954baf69877537f764b2cd
Erik Gärtner <erik@gartner.io>
2017-06-08 09:50:48 +0200

    Add ajax skeleton

--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,7 +1,22 @@
 'use strict';
 
+function queryPrometheus(url) {
+  $.ajax({
+    method: 'POST',
+    url: 'localhost:8080/check',
+    data: { url: url}
+  })
+  .done((msg) => {
+    document.getElementById('result').innerHTML = 'Got answer ' + msg;
+  })
+  .fail((msg) => {
+    document.getElementById('result').innerHTML = 'Got answer ' + msg;
+  });
+}
+
 $('#checkPage').on('click', () => {
   chrome.tabs.query({currentWindow: true, active: true}, (tabs) => {
-        document.getElementById('result').innerHTML = tabs[0].url;
+    document.getElementById('result').innerHTML = 'Checking... ' + tabs[0].url;
+    queryPrometheus(tabs[0].url);
   });
 });
